5baaf04aa615c31dbc9113a1acfb64d8ac780128
nawfaledu99 <nawfaledu99@gmail.com>
2026-08-21 16:16:31 +0800

    Add device model

---
+++ app/src/main/java/com/recgombak/batterymonitor/DeviceInfo.java
@@ -0,0 +1,2 @@
+package com.recgombak.batterymonitor;
+public class DeviceInfo { public String id; public String name; public String model; public int battery; public boolean charging; public long lastSeen; public String ip; }
